8226144a84f827963fd871d73623e3f2fd3865bd
Athesto <gamez.live@gmail.com>
2021-02-03 07:03:16 +0000

    main guard

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -3,25 +3,26 @@
 from requests import get
 from sys import argv
 
-api_users = "https://jsonplaceholder.typicode.com/users/{id}"
-api_todos = "https://jsonplaceholder.typicode.com/users/{id}/todos"
-csv_format = ['"{id}","{name}",', '"{completed}","{title}"']
-data = {
-    "id": argv[1],
-    "name": "",
-    "tasks": []
-}
-info = get(api_users.format(**data)).json()
-data.update(name=info.get('name'))
-info = get(api_todos.format(**data)).json()
-for element in info:
-    data['tasks'].append(
-        {
-            'completed': element.get('completed'),
-            'title': element.get('title')
-        })
-with open('{id}.csv'.format(**data), 'w') as f:
-    a = csv_format[0].format(**data)
-    for element in data["tasks"]:
-        b = csv_format[1].format(**element)
-        print(a+b, file=f)
+if __name__ == '__main__':
+    api_users = "https://jsonplaceholder.typicode.com/users/{id}"
+    api_todos = "https://jsonplaceholder.typicode.com/users/{id}/todos"
+    csv_format = ['"{id}","{name}",', '"{completed}","{title}"']
+    data = {
+        "id": argv[1],
+        "name": "",
+        "tasks": []
+    }
+    info = get(api_users.format(**data)).json()
+    data.update(name=info.get('name'))
+    info = get(api_todos.format(**data)).json()
+    for element in info:
+        data['tasks'].append(
+            {
+                'completed': element.get('completed'),
+                'title': element.get('title')
+            })
+    with open('{id}.csv'.format(**data), 'w') as f:
+        a = csv_format[0].format(**data)
+        for element in data["tasks"]:
+            b = csv_format[1].format(**element)
+            print(a+b, file=f)
