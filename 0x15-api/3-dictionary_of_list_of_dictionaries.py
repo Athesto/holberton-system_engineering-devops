4a04b233fd8f87996e77f6a0e8911e2ccbeff2b1
Athesto <gamez.live@gmail.com>
2021-02-03 09:23:57 +0000

    3-dictionary_of_list_of_dictionaries.py

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+'''export into json'''
+from json import dumps
+from requests import get
+from sys import argv
+
+if __name__ == '__main__':
+    api_users = "https://jsonplaceholder.typicode.com/users"
+    api_todos = "https://jsonplaceholder.typicode.com/todos"
+    user_data = {}
+    json_obj = {}
+
+    info = get(api_users).json()
+    for element in info:
+        tmp = {}
+        tmp['id'] = element.get('id')
+        tmp['username'] = element.get('username')
+        user_data[element.get('id')] = tmp
+        json_obj[element.get('id')] = []
+
+    print(dumps(user_data, indent=2))
+
+    info = get(api_todos).json()
+    for element in info:
+        my_id = element.get('userId')
+        json_obj[my_id].append(
+            {
+                'username': user_data.get(my_id).get('username'),
+                'completed': element.get('completed'),
+                'task': element.get('title')
+            })
+    print(dumps(json_obj, indent=2))
+    with open('todo_all_employees.json', 'w') as f:
+        print(dumps(json_obj), file=f)
