c82fe80a264be92a9c6d2dcf540cdca416400532
Athesto <gamez.live@gmail.com>
2021-02-03 07:59:46 +0000

    2-export_to_JSON.py

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''export into json'''
+from json import dumps
+from requests import get
+from sys import argv
+
+if __name__ == '__main__':
+    api_users = "https://jsonplaceholder.typicode.com/users/{id}"
+    api_todos = "https://jsonplaceholder.typicode.com/users/{id}/todos"
+    data = {
+        "id": argv[1],
+        "username": "",
+    }
+    json_obj = {
+        data.get('id'): []
+    }
+
+    info = get(api_users.format(**data)).json()
+    data.update(username=info.get('username'))
+    info = get(api_todos.format(**data)).json()
+    for element in info:
+        json_obj[data.get('id')].append(
+            {
+                'username': data.get('username'),
+                'completed': element.get('completed'),
+                'title': element.get('title')
+            })
+    with open('{id}.json'.format(**data), 'w') as f:
+        print(dumps(json_obj), file=f)
