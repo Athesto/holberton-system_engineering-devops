d98006bb84c7e4e9fe100cbfa2a71c265057a253
Athesto <gamez.live@gmail.com>
2021-02-03 06:01:55 +0000

    using both methods

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,25 +4,32 @@ using this REST API, for a given employee ID,
 returns information about his/her TODO list progress
 '''
 
-if __name__ == "__main__":
-    from requests import get
-    from sys import argv
 
-    id = argv[1]
-    api_users = "https://jsonplaceholder.typicode.com/users/{}"
-    api_todos = "https://jsonplaceholder.typicode.com/users/{}/todos"
+def main():
+    '''main file'''
+    getinfo("urllib")
+    # getinfo("requests")
+
+
+def getinfo(option):
+    '''get info'''
+    from sys import argv
+    methods = {'urllib': urllib_method, 'requests': requests_method}
+    api_users = "https://jsonplaceholder.typicode.com/users/{id}"
+    api_todos = "https://jsonplaceholder.typicode.com/users/{id}/todos"
     msg_title = "Employee {name} is done with tasks({done}/{total}):"
     msg_body = "\t {}"
-
     data = {
+        "id": argv[1],
         "name": "",
         "done": 0,
         "total": 0,
         "tasks": []
     }
-    data["name"] = get(api_users.format(id)).json().get("name")
-
-    for r in get(api_todos.format(id)).json():
+    info = methods[option](api_users, data)
+    data.update(name=info.get("name"))
+    info = methods[option](api_todos, data)
+    for r in info:
         data["total"] += 1
         if r.get("completed"):
             data["done"] += 1
@@ -30,3 +37,24 @@ if __name__ == "__main__":
     print(msg_title.format(**data))
     for task in data["tasks"]:
         print(msg_body.format(task))
+
+
+def requests_method(url, data):
+    '''this options works with requests module'''
+    from requests import get
+
+    return get(url.format(**data)).json()
+
+
+def urllib_method(url, data):
+    '''this option use urllib'''
+    from urllib import request
+    from json import loads
+
+    with request.urlopen(url.format(**data)) as r:
+        info = loads(r.read().decode())
+    return info
+
+
+if __name__ == "__main__":
+    main()
